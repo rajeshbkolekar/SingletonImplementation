f903a816002a4781e87e8618e7b4e3cc60ca5b61
Rajesh Kolekar <rajesh.kolekar@saviant.local>
2023-07-22 13:44:47 +0530

    first commit

---
+++ SingletonDesignPattern/SingletonDesignPattern/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace SingletonDesignPattern
+{
+    public class Logger
+    {
+        private static int _count;
+        private static Logger _instance;
+        static Mutex mutex = new Mutex();  
+
+        // first step so that no client can create innstace of object 
+        private Logger()
+        {
+           
+        }
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        // second step to make static method that can give logger instance 
+        public  static Logger GetLogger()
+        {
+            mutex.WaitOne();
+            if(_instance==null)
+            {
+                _count++;
+                Console.WriteLine("No of Instances" + _count);
+                _instance = new Logger();
+            }
+            mutex.ReleaseMutex();
+            return _instance;
+        }
+    }
+}
---
+++ SingletonDesignPattern/SingletonDesignPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace SingletonDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(LogUser1));
+            Thread thread2 = new Thread(new ThreadStart(LogUser2));
+            thread.Start();
+            thread2.Start();
+
+            thread.Join();
+            thread2.Join();
+        }
+
+        public static void LogUser1()
+        {
+            var logger1 = Logger.GetLogger();
+
+            logger1.Log("Message from logger1");
+
+        }
+
+        public static void LogUser2()
+        {
+            var logger1 = Logger.GetLogger();
+
+            logger1.Log("Message from logger2");
+
+        }
+    }
+}
